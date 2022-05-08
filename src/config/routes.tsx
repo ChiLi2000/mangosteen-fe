c7475255b677f0a6ba79a8aaac262116a836e009
chili.guo <chili.guo@pkfare.com>
2022-05-08 13:33:50 +0800

    完成4个欢迎页面

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,14 +1,20 @@
 import { welcome } from "../views/welcome";
 import { First } from "../components/Welcome/First";
+import { Second } from "../components/Welcome/Second";
+import { Forth } from "../components/Welcome/Third";
+import { Third } from "../components/Welcome/Forth";
 
 export const routes = [
   { path: '/', redirect: '/welcome' },
-    {
-      path: '/welcome',
-      component: welcome,
-      children: [
-        { path: '', redirect: '/welcome/1', },
-        { path: '1', component: First, }
-      ]
-    }
+  {
+    path: '/welcome',
+    component: welcome,
+    children: [
+      { path: '', redirect: '/welcome/1', },
+      { path: '1', component: First, },
+      { path: '2', component: Second, },
+      { path: '3', component: Third, },
+      { path: '4', component: Forth, },
+    ]
+  }
   ]
