5deaa6d90e5b77d89b654586327a4bfe58a00db7
chili.guo <chili.guo@pkfare.com>
2022-05-15 12:58:31 +0800

    制作 svg 雪碧图

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+// @ts-nocheck
+import { svgstore } from './src/vite_plugins/svgstore';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +12,7 @@ export default defineConfig({
       // options are passed on to @vue/babel-plugin-jsx
       transformOn: true,
       mergeProps: true
-    })]
+    }),
+    svgstore(),
+  ]
 })
