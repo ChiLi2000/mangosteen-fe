1667762c917cdda7b25c24da020bf69d2fcc3575
chili.guo <chili.guo@pkfare.com>
2022-05-15 15:03:22 +0800

    4个欢迎页支持滑动切换路由

--- src/components/Welcome/First.tsx
+++ src/components/Welcome/First.tsx
@@ -1,25 +1,13 @@
-import { defineComponent, ref, watchEffect } from 'vue';
+import { FunctionalComponent } from 'vue';
 import { Icon } from "../../shared/Icon";
-import { useSwipe } from '../../hooks/useSwipe';
-import { useRouter } from 'vue-router';
 import s from './welcomeCommon.module.scss';
-export const First = defineComponent({
-  setup() {
-    const div = ref<HTMLDivElement>()
-    const router = useRouter()
-    const { swiping, direction } = useSwipe(div, {
-      beforeStart: e => e.preventDefault()
-    })
-    watchEffect(() => {
-      if (swiping.value && direction.value === 'left') {
-        router.push('/welcome/2')
-      }
-    })
-    return () => (
-      <div class={s.card} ref={div}>
-        <Icon name="pig" />
-        <h2>会挣钱<br />还会省钱</h2>
-      </div>
-    )
-  }
-})
+export const First: FunctionalComponent = () => {
+  return (
+    <div class={s.card}>
+      <Icon name="pig" />
+      <h2>会挣钱<br />还会省钱</h2>
+    </div>
+  )
+}
+
+First.displayName = 'First'
--- src/views/welcome.tsx
+++ src/views/welcome.tsx
@@ -1,16 +1,37 @@
-import { defineComponent, Transition, VNode, ref, watchEffect } from 'vue';
-import { RouteLocationNormalizedLoaded, RouterView } from 'vue-router';
+import { defineComponent, ref, Transition, VNode, watchEffect } from 'vue';
+import { RouteLocationNormalizedLoaded, RouterView, useRoute, useRouter } from 'vue-router';
+import { useSwipe } from '../hooks/useSwipe';
 import s from './welcome.module.scss'
 import { Icon } from '../shared/Icon';
 export const welcome = defineComponent({
   setup: (props, context) => {
+    const main = ref<HTMLElement>()
+    const { direction, swiping } = useSwipe(main, { beforeStart: e => e.preventDefault() })
+    const route = useRoute()
+    const router = useRouter()
+    const push = () => {
+      if (route.name === 'Welcome1') {
+        router.push('/welcome/2')
+      } else if (route.name === 'Welcome2') {
+        router.push('/welcome/3')
+      } else if (route.name === 'Welcome3') {
+        router.push('/welcome/4')
+      } else if (route.name === 'Welcome4') {
+        router.push('/start')
+      }
+    }
+    watchEffect(() => {
+      if (swiping.value && direction.value === 'left') {
+        push()
+      }
+    })
     return () => 
     <div class={s.wrapper}>
       <header>
       <Icon name="mangosteen"/>
         <h1>山竹记账</h1>
       </header>
-      <main class={s.main}>
+      <main class={s.main} ref={main}>
         <RouterView name="main">
           {({ Component: X, route: R }: { Component: VNode, route: RouteLocationNormalizedLoaded }) =>
             <Transition enterFromClass={s.slide_fade_enter_from} enterActiveClass={s.slide_fade_enter_active}
