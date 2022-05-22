83dfbaf0313774fb3e369461ce8b407e319c3f44
chili.guo <chili.guo@pkfare.com>
2022-05-22 10:44:08 +0800

    创建 Tabs 组件

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -1,22 +1,31 @@
-import { defineComponent, PropType } from 'vue';
-import { MainLayout } from '../../layouts/MainLayout';
-import { Icon } from '../../shared/Icon';
-import s from './ItemCreate.module.scss';
+import { defineComponent, PropType, ref } from "vue";
+import { MainLayout } from "../../layouts/MainLayout";
+import { Icon } from "../../shared/Icon";
+import { Tabs, Tab } from "../../shared/Tabs";
+import s from "./ItemCreate.module.scss";
 export const ItemCreate = defineComponent({
   props: {
     name: {
-      type: String as PropType<string>
-    }
+      type: String as PropType<string>,
+    },
   },
   setup: (props, context) => {
+    const refKind = ref('支出')
     return () => (
-        <MainLayout>{{
-            title: () => '记一笔',
-            icon: () => <Icon name="left" class={s.navIcon} />,
-            default: ()=> <>
-              <div>main</div>
+      <MainLayout>
+        {{
+          title: () => "记一笔",
+          icon: () => <Icon name="left" class={s.navIcon} />,
+          default: () => (
+            <>
+              <Tabs v-model:selected={refKind.value}>
+                <Tab name="支出">icon 列表</Tab>
+                <Tab name="收入">icon 列表2</Tab>
+              </Tabs>
             </>
-        }}</MainLayout>
-    )
-  }
-})
+          ),
+        }}
+      </MainLayout>
+    );
+  },
+});
---
+++ src/shared/Tabs.tsx
@@ -0,0 +1,56 @@
+import { defineComponent, PropType } from "vue";
+import s from "./Tabs.module.scss";
+export const Tabs = defineComponent({
+  props: {
+    selected: {
+      type: String as PropType<string>,
+      required: false,
+    },
+    onUpdateSelected: {
+      type: Function as PropType<(name: string) => void>,
+      required: false,
+    },
+  },
+  setup: (props, context) => {
+    return () => {
+      const tabs = context.slots.default?.();
+      console.log(tabs);
+      if (!tabs) return () => null;
+      for (let i = 0; i < tabs.length; i++) {
+        if (tabs[i].type !== Tab) {
+          throw new Error("<Tabs> only accepts <Tab> as children");
+        }
+      }
+      return (
+        <div class={s.tabs}>
+          <ol class={s.tabs_nav}>
+            {tabs.map((item) => (
+              <li
+                class={item.props?.name === props.selected ? s.selected : ""}
+                onClick={() =>
+                  context.emit("update:selected", item.props?.name)
+                }
+              >
+                {item.props?.name}
+              </li>
+            ))}
+          </ol>
+          <div>
+          {tabs.find(item => item.props?.name === props.selected)}
+          </div>
+        </div>
+      );
+    };
+  },
+});
+
+export const Tab = defineComponent({
+  props: {
+    name: {
+      type: String as PropType<string>,
+    },
+  },
+  setup: (props, context) => {
+    return () => <div>{context.slots.default?.()}</div>;
+  },
+});
