25bc38af9a187f9a4e3dd8cb18f421afedb9400f
chili.guo <chili.guo@pkfare.com>
2022-05-29 16:54:25 +0800

    金额输入最多允许两位小数

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -2,7 +2,7 @@ import { defineComponent, PropType, ref } from 'vue';
 import { Icon } from '../../shared/Icon';
 import { time } from '../../shared/time';
 import s from './InputPad.module.scss';
-import { DatetimePicker, NumberKeyboard, Popup } from 'vant';
+import { DatetimePicker, Popup } from 'vant';
 export const InputPad = defineComponent({
   props: {
     name: {
@@ -12,12 +12,33 @@ export const InputPad = defineComponent({
   setup: (props, context) => {
     const now = new Date()
     const refDate = ref<Date>(now)
-    const refDatePickerVisible = ref(false)
-    const showDatePicker = () => refDatePickerVisible.value = true
-    const hideDatePicker = () => refDatePickerVisible.value = false
-    const setDate = (date: Date) => { refDate.value = date; hideDatePicker() }
-    const refAmount = ref('')
-    const appendText = (n: number | string) => refAmount.value += n.toString()
+    const refAmount = ref('0')
+    const appendText = (n: number | string) => {
+      const nString = n.toString()
+      const dotIndex = refAmount.value.indexOf('.')
+      if (refAmount.value.length >= 13) {
+        return
+      }
+      if (dotIndex >= 0 && refAmount.value.length - dotIndex > 2) {
+        return
+      }
+      if (nString === '.') {
+        if (dotIndex >= 0) { // 已经有小数点了
+          return
+        }
+      } else if (nString === '0') {
+        if (dotIndex === -1) { // 没有小数点
+          if (refAmount.value === '0') { // 没小数点，但是有0
+            return
+          }
+        }
+      } else {
+        if (refAmount.value === '0') {
+          refAmount.value = ''
+        }
+      }
+      refAmount.value += n.toString()
+    }
     const buttons = [
       { text: '1', onClick: () => { appendText(1) } },
       { text: '2', onClick: () => { appendText(2) } },
@@ -30,9 +51,13 @@ export const InputPad = defineComponent({
       { text: '9', onClick: () => { appendText(9) } },
       { text: '.', onClick: () => { appendText('.') } },
       { text: '0', onClick: () => { appendText(0) } },
-      { text: '清空', onClick: () => { } },
+      { text: '清空', onClick: () => { refAmount.value = '0' } },
       { text: '提交', onClick: () => { } },
     ]
+    const refDatePickerVisible = ref(false)
+    const showDatePicker = () => refDatePickerVisible.value = true
+    const hideDatePicker = () => refDatePickerVisible.value = false
+    const setDate = (date: Date) => { refDate.value = date; hideDatePicker() }
     return () => <>
       <div class={s.dateAndAmount}>
         <span class={s.date}>
