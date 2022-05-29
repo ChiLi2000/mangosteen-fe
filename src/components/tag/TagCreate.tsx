186511185665a6584d1454394fc61a309a0e5fec
chili.guo <chili.guo@pkfare.com>
2022-05-29 22:43:26 +0800

    完成 EmojiSelect组件

--- src/components/tag/TagCreate.tsx
+++ src/components/tag/TagCreate.tsx
@@ -1,18 +1,44 @@
-import { defineComponent } from 'vue';
-import { MainLayout } from '../../layouts/MainLayout';
-import { Icon } from '../../shared/Icon';
+import { defineComponent, reactive } from "vue";
+import { MainLayout } from "../../layouts/MainLayout";
+import { Button } from "../../shared/Button";
+import { EmojiSelect } from "../../shared/EmojiSelect";
+import { Icon } from "../../shared/Icon";
+import s from "./TagCreate.module.scss";
 export const TagCreate = defineComponent({
-  setup: (props,context) => {
+  setup: (props, context) => {
+      const formData= reactive({
+          name:'',
+          sign:'',
+      })
     return () => (
-      <MainLayout>{
-          {
-              title:()=>'新建标签',
-              icon: ()=><Icon name='left' />,
-              default:()=>(
-                  <div>new</div>
-              )
-          }
-          }</MainLayout>
-    )
-  }
-})
+      <MainLayout>
+        {{
+          title: () => "新建标签",
+          icon: () => <Icon name="left" />,
+          default: () => (
+            <form>
+              <div>
+                <label>
+                  <span>标签名</span>
+                  <input />
+                </label>
+              </div>
+              <div>
+                <label>
+                  <span>{'符号 ' + formData.sign}</span>
+                  <EmojiSelect v-model={formData.sign}/>
+                </label>
+              </div>
+              <div>
+              <p class={s.tips}>记账时长按标签即可进行编辑</p>
+              </div>
+              <div>
+              <Button class={s.button}>确定</Button>
+              </div>
+            </form>
+          ),
+        }}
+      </MainLayout>
+    );
+  },
+});
