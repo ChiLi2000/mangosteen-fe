64ae924e54cb35f66286a1e52af700eaf961f498
chili.guo <chili.guo@pkfare.com>
2022-06-08 22:23:51 +0800

    Button组件添加type

--- src/shared/Button.tsx
+++ src/shared/Button.tsx
@@ -1,21 +1,24 @@
-import { defineComponent, PropType } from 'vue';
-import s from './Button.module.scss';
+import { defineComponent, PropType } from "vue";
+import s from "./Button.module.scss";
 
 export const Button = defineComponent({
   props: {
     onClick: {
-      type: Function as PropType<(e: MouseEvent) => void>
+      type: Function as PropType<(e: MouseEvent) => void>,
     },
     level: {
-      type: String as PropType<'important' | 'normal' | 'danger'>,
-      default: 'important'
+      type: String as PropType<"important" | "normal" | "danger">,
+      default: "important",
+    },
+    type: {
+      type: String as PropType<"submit" | "button">,
     },
   },
   setup: (props, context) => {
     return () => (
-      <button class={[s.button, s[props.level]]}>
+      <button type={props.type} class={[s.button, s[props.level]]}>
         {context.slots.default?.()}
       </button>
-    )
-  }
-})
+    );
+  },
+});
