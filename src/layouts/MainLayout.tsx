3ebb4cc9ca295e1bb939dd15e6f12c5692199422
chili.guo <chili.guo@pkfare.com>
2022-06-09 23:05:58 +0800

    使用sticky固定顶部

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,17 +1,18 @@
 import { defineComponent, PropType } from 'vue';
 import { Navbar } from '../shared/Navbar';
+import s from './MainLayout.module.scss';
 export const MainLayout = defineComponent({
   setup: (props, context) => {
     return () => (
-      <div>
-      <Navbar>{
-        {
-          default: () => context.slots.title?.(),
-          icon: () => context.slots.icon?.(),
-        }
-      }</Navbar>
-      {context.slots.default?.()}
+      <div class={s.wrapper}>
+        <Navbar class={s.navbar}>{
+          {
+            default: () => context.slots.title?.(),
+            icon: () => context.slots.icon?.(),
+          }
+        }</Navbar>
+        {context.slots.default?.()}
       </div>
     )
   }
-}) 
+})
