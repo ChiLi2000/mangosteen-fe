ae7756161de3b54067d528f296a75dbaff3d3f98
chili.guo <chili.guo@pkfare.com>
2022-07-31 22:03:24 +0800

    长按tag跳转到编辑页面

--- src/components/item/Tags.tsx
+++ src/components/item/Tags.tsx
@@ -1,5 +1,5 @@
-import { defineComponent, onUpdated, PropType } from "vue";
-import { RouterLink } from "vue-router";
+import { defineComponent, PropType, ref } from "vue";
+import { RouterLink, useRouter } from "vue-router";
 import { Button } from "../../shared/Button";
 import { http } from "../../shared/Http";
 import { Icon } from "../../shared/Icon";
@@ -15,6 +15,7 @@ export const Tags = defineComponent({
   },
   emits: ["update:selected"],
   setup: (props, context) => {
+    const router = useRouter();
     const { tags, hasMore, page, fetchTags } = useTags((page) => {
       return http.get<Resources<Tag>>("/tags", {
         kind: props.kind,
@@ -25,9 +26,38 @@ export const Tags = defineComponent({
     const onSelect = (tag: Tag) => {
       context.emit("update:selected", tag.id);
     };
+    const timer = ref<number>();
+    const currentTag = ref<HTMLDivElement>();
+
+    const onLongPress = (tagId: Tag["id"]) => {
+      router.push(
+        `/tags/${tagId}/edit?kind=${props.kind}&return_to=${router.currentRoute.value.fullPath}`
+      );
+    };
+    const onTouchStart = (e: TouchEvent, tag: Tag) => {
+      currentTag.value = e.currentTarget as HTMLDivElement;
+      timer.value = setTimeout(() => {
+        onLongPress(tag.id);
+      }, 500);
+    };
+    const onTouchEnd = (e: TouchEvent) => {
+      clearTimeout(timer.value);
+    };
+    const onTouchMove = (e: TouchEvent) => {
+      const pointedElement = document.elementFromPoint(
+        e.touches[0].clientX,
+        e.touches[0].clientY
+      );
+      if (
+        currentTag.value !== pointedElement &&
+        currentTag.value?.contains(pointedElement) === false
+      ) {
+        clearTimeout(timer.value);
+      }
+    };
     return () => (
       <>
-        <div class={s.tags_wrapper}>
+        <div class={s.tags_wrapper} onTouchmove={onTouchMove}>
           <RouterLink to={`/tags/create?kind=${props.kind}`} class={s.tag}>
             <div class={s.sign}>
               <Icon name="add" class={s.createTag} />
@@ -38,6 +68,8 @@ export const Tags = defineComponent({
             <div
               class={[s.tag, props.selected === tag.id ? s.selected : ""]}
               onClick={() => onSelect(tag)}
+              onTouchstart={(e) => onTouchStart(e, tag)}
+              onTouchend={onTouchEnd}
             >
               <div class={s.sign}>{tag.sign}</div>
               <div class={s.name}>{tag.name}</div>
