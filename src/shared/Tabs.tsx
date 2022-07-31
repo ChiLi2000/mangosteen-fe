90b17587de8228509b2ea21e0bc6f7c9e061e02a
chili.guo <chili.guo@pkfare.com>
2022-07-31 21:15:23 +0800

    封装 Tags 组件

--- src/shared/Tabs.tsx
+++ src/shared/Tabs.tsx
@@ -1,55 +1,63 @@
-import { defineComponent, PropType } from 'vue';
-import s from './Tabs.module.scss';
+import { defineComponent, PropType } from "vue";
+import s from "./Tabs.module.scss";
 export const Tabs = defineComponent({
   props: {
     classPrefix: {
-      type: String
+      type: String,
     },
     selected: {
       type: String as PropType<string>,
       required: false,
     },
   },
-  emits: ['update:selected'],
+  emits: ["update:selected"],
   setup: (props, context) => {
     return () => {
-      const tabs = context.slots.default?.()
-      if (!tabs) return () => null
+      const tabs = context.slots.default?.();
+      if (!tabs) return () => null;
       for (let i = 0; i < tabs.length; i++) {
         if (tabs[i].type !== Tab) {
-          throw new Error('<Tabs> only accepts <Tab> as children')
+          throw new Error("<Tabs> only accepts <Tab> as children");
         }
       }
-      const cp = props.classPrefix
-      return <div class={[s.tabs, cp + '_tabs']}>
-        <ol class={[s.tabs_nav, cp + '_tabs_nav']}>
-          {tabs.map(item =>
-            <li class={[
-              item.props?.name === props.selected ? [s.selected, cp + '_selected'] : '',
-              cp + '_tabs_nav_item'
-            ]}
-              onClick={() => context.emit('update:selected', item.props?.name)}
-            >
-              {item.props?.name}
-            </li>)}
-        </ol>
-        <div>
-          {tabs.find(item => item.props?.name === props.selected)}
+      const cp = props.classPrefix;
+      return (
+        <div class={[s.tabs, cp + "_tabs"]}>
+          <ol class={[s.tabs_nav, cp + "_tabs_nav"]}>
+            {tabs.map((item) => (
+              <li
+                class={[
+                  item.props?.name === props.selected
+                    ? [s.selected, cp + "_selected"]
+                    : "",
+                  cp + "_tabs_nav_item",
+                ]}
+                onClick={() =>
+                  context.emit("update:selected", item.props?.name)
+                }
+              >
+                {item.props?.name}
+              </li>
+            ))}
+          </ol>
+          <div>
+            {tabs.map((item) => (
+              <div v-show={item.props?.name === props.selected}>{item}</div>
+            ))}
+          </div>
         </div>
-      </div>
-    }
-  }
-})
+      );
+    };
+  },
+});
 
 export const Tab = defineComponent({
   props: {
     name: {
-      type: String as PropType<string>
-    }
+      type: String as PropType<string>,
+    },
   },
   setup: (props, context) => {
-    return () => (
-      <div>{context.slots.default?.()}</div>
-    )
-  }
-})
+    return () => <div>{context.slots.default?.()}</div>;
+  },
+});
