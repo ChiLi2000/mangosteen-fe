f16030b621a7b66786218135f9057403a15a9148
chili.guo <chili.guo@pkfare.com>
2022-08-02 11:19:47 +0800

    金额处理为两位小数

--- src/components/item/ItemSummary.tsx
+++ src/components/item/ItemSummary.tsx
@@ -2,6 +2,7 @@ import { Button } from "vant";
 import { defineComponent, onMounted, PropType, ref } from "vue";
 import { FloatButton } from "../../shared/FloatButton";
 import { http } from "../../shared/Http";
+import { Money } from "../../shared/Money";
 import s from "./ItemSummary.module.scss";
 export const ItemSummary = defineComponent({
   props: {
@@ -63,7 +64,7 @@ export const ItemSummary = defineComponent({
                     <div class={s.tagAndAmount}>
                       <span class={s.tag}>{item.tags_id[0]}</span>
                       <span class={s.amount}>
-                        ￥<>{item.amount}</>
+                        ￥<Money value={item.amount} />
                       </span>
                     </div>
                     <div class={s.time}>{item.happen_at}</div>
