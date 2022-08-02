d9f75be8680bbdb4061f2c44af8ac38d6dba92f8
chili.guo <chili.guo@pkfare.com>
2022-08-02 15:13:14 +0800

    重构

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -29,31 +29,21 @@ export const Charts = defineComponent({
       if (!props.startDate || !props.endDate) {
         return [];
       }
-      const array = [];
       const diff =
         new Date(props.endDate).getTime() - new Date(props.startDate).getTime();
       const n = diff / DAY + 1;
-      let data1Index = 0;
-      for (let i = 0; i < n; i++) {
+      return Array.from({ length: n }).map((_, i) => {
         const time = new Time(props.startDate + "T00:00:00.000+0800")
           .add(i, "day")
           .getTimestamp();
-        if (
-          data1.value[data1Index] &&
-          new Date(data1.value[data1Index].happen_at).getTime() === time
-        ) {
-          array.push([
-            new Date(time).toISOString(),
-            data1.value[data1Index].amount,
-          ]);
-          data1Index += 1;
-        } else {
-          array.push([new Date(time).toISOString(), 0]);
-        }
-      }
-      return array as [string, number][];
+        const item = data1.value[0];
+        const amount =
+          item && new Date(item.happen_at).getTime() === time
+            ? data1.value.shift()!.amount
+            : 0;
+        return [new Date(time).toISOString(), amount];
+      });
     });
-
     onMounted(async () => {
       const response = await http.get<{ groups: Data1; summary: number }>(
         "/items/summary",
