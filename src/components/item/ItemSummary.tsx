ccd054dd7bea501449056883d92568addaaaab82
chili.guo <chili.guo@pkfare.com>
2022-08-03 21:05:34 +0800

    封装 userAfterMe，消除一些警告

--- src/components/item/ItemSummary.tsx
+++ src/components/item/ItemSummary.tsx
@@ -1,12 +1,12 @@
 import {
   defineComponent,
-  onMounted,
   PropType,
   reactive,
   ref,
   watch,
 } from "vue";
 import { RouterLink } from "vue-router";
+import { useAfterMe } from "../../hooks/useAfterMe";
 import { Button } from "../../shared/Button";
 import { Center } from "../../shared/Center";
 import { Datetime } from "../../shared/Datetime";
@@ -49,7 +49,7 @@ export const ItemSummary = defineComponent({
         (pager.page - 1) * pager.per_page + resources.length < pager.count;
       page.value += 1;
     };
-    onMounted(fetchItems);
+    useAfterMe(fetchItems);
     watch(
       () => [props.startDate, props.endDate],
       () => {
@@ -79,7 +79,7 @@ export const ItemSummary = defineComponent({
       );
       Object.assign(itemsBalance, response.data);
     };
-    onMounted(fetchItemsBalance);
+    useAfterMe(fetchItemsBalance);
     watch(
       () => [props.startDate, props.endDate],
       () => {
