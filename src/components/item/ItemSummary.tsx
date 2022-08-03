cd437aaf8ac8b6f7f475e3a77c47dabfeeb67c4b
chili.guo <chili.guo@pkfare.com>
2022-08-03 21:18:07 +0800

    使用 store.$reset() 代替 reset

--- src/components/item/ItemSummary.tsx
+++ src/components/item/ItemSummary.tsx
@@ -38,7 +38,7 @@ export const ItemSummary = defineComponent({
     watch(
       () => [props.startDate, props.endDate],
       () => {
-        itemStore.reset();
+        itemStore.$reset();
         itemStore.fetchItems();
       }
     );
