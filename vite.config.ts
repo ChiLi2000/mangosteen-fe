f7c7f3d5b38eab3b5a4739039a674705792fce6e
chili.guo <chili.guo@pkfare.com>
2022-08-05 15:48:23 +0800

    重置服务

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   server: {
     proxy:{
       '/api/v1':{
-        target:'http://121.196.236.94:3000/'
+        target:'http://120.77.156.197:3000/'
       }
     }
   }
